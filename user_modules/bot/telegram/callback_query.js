baf31ca5c2b4e54515d5245a35a5c5be28e4fbd8
Mohit <mohitkashyap46707@gmail.com>
2023-10-15 00:16:23 +0530

    Hey

--- user_modules/bot/telegram/callback_query.js
+++ user_modules/bot/telegram/callback_query.js
@@ -1,8 +1,18 @@
-function CallbackQuery(req, res) {
-  const yt_api = "https://flask-api.cyclic.app/yt",
-  params = {
-    url: ""
+const send = require("./send");
+
+async function CallbackQuery(req, res) {
+  const data = req.body;
+  const callback_query = data["callback_query"];
+  const chat_id = callback_query["from"]["id"];
+  const message_id = callback_query["message"]["message_id"];
+  const params = {
+    chat_id,
+    message_id,
+    text: data
   };
+  
+  await send("editMessageText", params);
+  res.send("True");
 }
 
 module.exports = CallbackQuery;
--- user_modules/bot/telegram/message.js
+++ user_modules/bot/telegram/message.js
@@ -8,7 +8,15 @@ async function Message(req, res) {
   const message_id = message["message_id"];
   const chat_id = message["from"]["id"];
   const text = message["text"];
-  const params = { chat_id, text: data };
+  const params = { chat_id, text: "Select handler!", reply_to_message_id: message_id };
+  
+  const reply_markup = {
+    inline_keyboard: [
+      [{ text: "YouTube", callback_data: "YT" }]
+    ]
+  };
+  
+  params["reply_markup"] = JSON.stringify(reply_markup);
   
   // sending response
   await send("sendMessage", params);
