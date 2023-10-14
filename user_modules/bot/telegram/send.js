9d9729d0173f57d59184062b7d492d0f825a7206
Mohit <mohitkashyap46707@gmail.com>
2023-10-14 13:10:11 +0530

    send has changed to sync

--- user_modules/bot/telegram/send.js
+++ user_modules/bot/telegram/send.js
@@ -1,9 +1,9 @@
-function send(method, params) {
+async function send(method, params) {
   const axios = require("axios"),
   TOKEN = process.env.bot_token,
   url = `https://api.telegram.org/bot${TOKEN}/${method}`;
   
-  axios.post(url, params)
+  await axios.post(url, params)
   .then(data => {
     const tg_data = JSON.parse(data);
     if (!tg_data["ok"]) console.log(tg_data);
